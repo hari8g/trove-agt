b5a0cdcc1e3244020eb6dac9f0b88aee8f876b22
Hariprasad G <harig@Hariprasads-Laptop.local>
2025-02-16 16:42:25 +0530

    Removed API key from history and added .env support

--- utils/config.py
+++ utils/config.py
@@ -1,4 +1,9 @@
 import yaml
+import os
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
 
 class Config:
     def __init__(self, config_path="config/config.yaml"):
@@ -6,6 +11,9 @@ class Config:
             self.config = yaml.safe_load(file)
 
     def get(self, key):
+        if key == "openai":
+            self.config[key]["api_key"] = os.getenv("OPENAI_API_KEY")  # Load from .env
         return self.config.get(key)
 
 CONFIG = Config()
+
